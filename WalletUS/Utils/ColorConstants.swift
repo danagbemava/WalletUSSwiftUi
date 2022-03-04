f6bc01b39fd3c50978aac70f2364d863ce4cbdc5
danagbemava <danagbemava@gmail.com>
2022-03-04 19:36:41 +0000

    Initial Commit

---
+++ WalletUS/Utils/ColorConstants.swift
@@ -0,0 +1,18 @@
+//
+//  ColorConstants.swift
+//  WalletUS
+//
+//  Created by Daniel Agbemava on 04/03/2022.
+//
+
+import Foundation
+import SwiftUI
+
+
+struct ColorConstants {
+    
+    let primaryBlue = Color(hex: "#3862F8")
+    let fadedBlue = Color(hex: "#E7EEEFE")
+    let customBlack = Color(hex: "#060F27")
+    let customGrey = Color(hex: "#989898")
+}
---
+++ WalletUS/WalletUSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletUSApp.swift
+//  WalletUS
+//
+//  Created by Daniel Agbemava on 04/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct WalletUSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
